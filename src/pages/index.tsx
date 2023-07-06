8d5bfc4c9038fe3ab9916a539f26f1174fd0591d
Enrique Rimando <elrmndo@icloud.com>
2023-07-06 08:45:54 +0800

    fix: update title, content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,28 +4,23 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Enrique Rimando</title>
+        <title>Enrique Rimando | Making things possible with code</title>
         <meta name="description" content="Enrique Rimando" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col min-h-screen text-center selection:text-white selection:bg-sky-300">
         <section className="flex-1 flex flex-col justify-center items-center bg-slate-50 px-4">
-          <h1 className="text-4xl font-bold">Enrique Rimando.</h1>
+          <h1 className="text-4xl font-bold">Enrique Rimando</h1>
           <p className="text-lg text-gray-500">
-            Hello, kamusta? Enrique here. I am a Full Stack Developer with 5+
-            years of progressive experience
+            Got questions? Send me an email:{' '}
+            <a
+              className="text-cyan-500 text-bold underline"
+              href="mailto:hi@elrmndo.dev"
+            >
+              hi@elrmndo.dev
+            </a>
           </p>
-
-          <ul
-            className="mt-6 text-gray-500 text-sm flex flex-row space-x-3"
-            aria-label="skills"
-          >
-            <li>TypeScript</li>
-            <li>React.js</li>
-            <li>Node.js</li>
-            <li>Tailwind CSS</li>
-          </ul>
         </section>
 
         <footer className="p-6 text-xs uppercase border-t border-slate-100">
