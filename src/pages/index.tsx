6cf708581db7c068b4b99b0edc2a9940762079d7
Enrique Rimando <me@elrmndo.dev>
2025-03-17 11:27:13 +0800

    chore: remove every text except email

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,7 @@ export default function Home() {
 
       <main className="flex flex-col min-h-screen text-center selection:text-white selection:bg-sky-300">
         <section className="flex-1 flex flex-col justify-center items-center bg-slate-50 px-4">
-          <h1 className="text-4xl font-bold">
-            Hi, I&apos;m Enrique Rimando ✨
-          </h1>
           <p className="text-lg text-gray-500">
-            You can contact me at{' '}
             <a
               className="text-cyan-500 text-bold underline"
               href="mailto:hi@elrmndo.dev"
